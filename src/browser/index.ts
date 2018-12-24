1161fe9aa8792a8a9db88ce71cf0c8e92b192d6f
Andrea <andry.tino@gmail.com>
2018-12-24 18:54:41 +0100

    Working inner Typescript for electron

--- src/browser/index.ts
+++ src/browser/index.ts
@@ -1,3 +1,5 @@
+const os = require("os");
+
 window.addEventListener("load", (e) => {
-    document.body.textContent = "Hi";
+    document.body.textContent = "Hi " + os.constants.UV_UDP_REUSEADDR;
 });
